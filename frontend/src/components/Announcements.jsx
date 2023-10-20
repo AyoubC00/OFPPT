1fe87a775d6e39409fa749a306a514855c9b438e
Ayoub <chchoukerayoub@gmail.com>
2023-10-20 17:06:27 +0100

    Fix misplaced pagination

--- frontend/src/components/Announcements.jsx
+++ frontend/src/components/Announcements.jsx
@@ -13,9 +13,7 @@ const Announcements = () => {
                 Announcements
             </Typography>
             <div className="px-4 sm:px-16 lg:grid lg:grid-rows-1 lg:grid-cols-5 lg:gap-x-8 xl:gap-x-8 xl:w-5/6 xl:mx-auto">
-                <div className="row-start-1 row-end-1 col-start-3 col-end-6 mb-2">
-                    <Pagination />
-                </div>
+                
                 <div className="overflow-y-auto h-674 mb-16 bg-white lg:mb-0 lg:col-span-2 row-start-2 row-end-3">
                     <Typography variant="h4" className="bg-white p-6 sticky top-0 z-50 shadow-sm">Pinned</Typography>
                     <PinnedAnnouncement />
@@ -26,6 +24,9 @@ const Announcements = () => {
                     <PinnedAnnouncement />
                     <PinnedAnnouncement />
                     {/* <NoResult /> */}
+                </div>
+                <div className="row-start-1 row-end-1 col-start-3 col-end-6 mb-2">
+                    <Pagination />
                 </div>
                 <div className="col-span-3 row-start-2 row-end-3">
                     <div className="flex flex-col gap-2">
