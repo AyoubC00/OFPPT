1e57f530ba00034092829ebed27d63c6678a6150
AyoubC00 <118103600+AyoubC00@users.noreply.github.com>
2023-10-27 19:23:33 +0000

    New Updates to hero & myway

--- frontend/src/features/hero/Hero.jsx
+++ frontend/src/features/hero/Hero.jsx
@@ -3,7 +3,7 @@ import ofpptFront from "../../assets/ofppt.jpg"
 const Hero = () =>
 {
     return (
-        <div className="relative h-[35rem] overflow-hidden">
+        <div className="relative max-h-[35rem] overflow-hidden">
             <img src={ ofpptFront } alt="OFPPT Front image" className="w-full object-fill" />
             <div className="bg-blue-gray-800 absolute z-30 top-0 left-0 w-full h-full mix-blend-overlay"></div>
         </div>
