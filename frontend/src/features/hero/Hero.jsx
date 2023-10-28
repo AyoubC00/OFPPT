8a2ced8f34e3e9983f954403b43c05e87e5d069b
AyoubC00 <118103600+AyoubC00@users.noreply.github.com>
2023-10-28 07:51:29 +0000

    Update hero img

--- frontend/src/features/hero/Hero.jsx
+++ frontend/src/features/hero/Hero.jsx
@@ -4,7 +4,7 @@ const Hero = () =>
 {
     return (
         <div className="relative max-h-[35rem] overflow-hidden">
-            <img src={ ofpptFront } alt="OFPPT Front image" className="w-full object-fill" />
+            <img src={ ofpptFront } alt="OFPPT Front image" className="w-full object-fill translate-y-10 md:translate-y-0" />
             <div className="bg-blue-gray-800 absolute z-30 top-0 left-0 w-full h-full mix-blend-overlay"></div>
         </div>
     )
