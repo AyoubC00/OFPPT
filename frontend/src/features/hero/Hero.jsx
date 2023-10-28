833f6aa59174d08b4ec9acedce89aa025299f84c
AyoubC00 <118103600+AyoubC00@users.noreply.github.com>
2023-10-28 08:00:05 +0000

    Update hero img

--- frontend/src/features/hero/Hero.jsx
+++ frontend/src/features/hero/Hero.jsx
@@ -3,8 +3,8 @@ import ofpptFront from "../../assets/ofppt.jpg"
 const Hero = () =>
 {
     return (
-        <div className="relative max-h-[35rem] overflow-hidden">
-            <img src={ ofpptFront } alt="OFPPT Front image" className="w-full object-fill translate-y-10 md:translate-y-0" />
+        <div className="relative max-h-[35rem] xl:max-h-[45rem] overflow-hidden">
+            <img src={ ofpptFront } alt="OFPPT Front image" className="w-full translate-y-20 lg:-translate-y-10 object-cover" />
             <div className="bg-blue-gray-800 absolute z-30 top-0 left-0 w-full h-full mix-blend-overlay"></div>
         </div>
     )
