271db43802e39617c8bbf4b11c54c4623c139fde
Ouaissa <abdououaissa46@gmail.com>
2024-03-13 14:23:36 +0000

    Add migration for cours table

--- backend/database/migrations/2024_03_13_142217_create_cours_table.php
+++ backend/database/migrations/2024_03_13_142217_create_cours_table.php
@@ -8,12 +8,23 @@ return new class extends Migration
 {
     /**
      * Run the migrations.
+     *
      */
     public function up(): void
     {
         Schema::create('cours', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->unsignedBigInteger('competence_id');
+            $table->string('title');
+            $table->string('content')->nullable();
+            $table->string('content_type')->nullable();
+            $table->string('file_name')->nullable();
+            $table->string('video_url')->nullable();
+            $table->string('poster_url')->nullable();
+            $table->timestamp('created_at')->nullable();
+            $table->timestamp('updated_at')->nullable();
+            $table->integer('level')->nullable();
+            $table->foreign('competence_id')->references('code')->on('competences')->onDelete('cascade')->onUpdate('restrict');
         });
     }
 
