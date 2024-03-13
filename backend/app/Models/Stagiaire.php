592fa6430ce565e8084ee09cf18d585cb0a5e06a
Ayoub Chouker <chchoukerayoub@gmail.com>
2024-03-13 09:44:21 +0100

    Renove duplicate user method for stagiaire model

--- backend/app/Models/Stagiaire.php
+++ backend/app/Models/Stagiaire.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Club;
+use App\Models\User;
 
 
 class Stagiaire extends Model
@@ -13,12 +15,9 @@ class Stagiaire extends Model
     {
         return $this->belongsTo(Groupe::class,'id','groupe_id');
     }
-
-    public function user()
-    {
-        return $this->hasOne(User::class,'id','user_id');
+    public function user () {
+        return $this->belongsTo(User::class,'id','user_id');
     }
-
     public function filiere()
     {
         return $this->belongsTo(Filiere::class,'id','filiere_id');
