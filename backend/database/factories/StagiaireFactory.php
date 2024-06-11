460fc3d568f314957d3fc45d3dd39e8aa8e006a8
khaoula <khaoula.esioudi@gmail.com>
2024-06-11 18:54:33 +0100

    changes in seeders

--- backend/database/factories/StagiaireFactory.php
+++ backend/database/factories/StagiaireFactory.php
@@ -3,12 +3,18 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Filiere;
+use App\Models\Groupe;
+use App\Models\User;
+use App\Models\Stagiaire;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Stagiaire>
  */
 class StagiaireFactory extends Factory
 {
+    protected $model = Stagiaire::class;
+
     /**
      * Define the model's default state.
      *
@@ -16,14 +22,12 @@ class StagiaireFactory extends Factory
      */
     public function definition(): array
     {
-        $filiere = \App\Models\Filiere::factory()->create();
+        $filiere = Filiere::factory()->create();
         return [
             'cef' => $this->faker->unique()->regexify('[A-Z]{5}[0-9]{3}'),
-            'groupe_id' => \App\Models\Groupe::factory(['filiere_id'=>$filiere->id]),
+            'groupe_id' => Groupe::factory()->create(['filiere_id' => $filiere->id])->id,
             'filiere_id' => $filiere->id,
-            'user_id' => function () {
-                return \App\Models\User::factory()->create()->id;
-            },
+            'user_id' => User::factory()->create(['role' => 'stagiaire'])->id,
         ];
     }
 }
--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -10,6 +10,7 @@ use \App\Models\Groupe;
 use \App\Models\Option;
 use \App\Models\Question;
 use \App\Models\Quiz;
+use App\Models\Stagiaire;
 
 class DatabaseSeeder extends Seeder
 {
@@ -19,68 +20,94 @@ class DatabaseSeeder extends Seeder
 
     public function run(): void
     {
+        // User::factory(20)->create();
+
+        // User::factory()->create(
+        //     [
+        //         "prenom" => "john",
+        //         "nom" => "doe",
+        //         "email" => "johndoe@gmail.com",
+        //         "password" => "123456",
+        //         "role" => "administrateur",
+        //         "cin" => "JA12345",
+        //     ]
+        // );
+        // User::factory()->create(
+        //     [
+        //         "prenom" => "jane",
+        //         "nom" => "doe",
+        //         "email" => "janedoe@gmail.com",
+        //         "password" => "123456",
+        //         "role" => "administrateur",
+        //         "cin" => "JA67890",
+        //     ],
+        // );
+
+        // $stagiaires = User::where('role', 'stagiaire')->get();
+        // foreach ($stagiaires as $stagiaire) {
+        //     \App\Models\Stagiaire::factory()->create();
+        // }
+        // Seed users
         User::factory(20)->create();
 
-        User::factory()->create(
-            [
-                "prenom" => "john",
-                "nom" => "doe",
-                "email" => "johndoe@gmail.com",
-                "password" => "123456",
-                "role" => "administrateur",
-                "cin" => "JA12345",
-            ]
-        );
-        User::factory()->create(
-            [
-                "prenom" => "jane",
-                "nom" => "doe",
-                "email" => "janedoe@gmail.com",
-                "password" => "123456",
-                "role" => "administrateur",
-                "cin" => "JA67890",
-            ],
-        );
+        User::factory()->create([
+            "prenom" => "john",
+            "nom" => "doe",
+            "email" => "johndoe@gmail.com",
+            "password" => bcrypt("123456"),  // Use bcrypt for password
+            "role" => "administrateur",
+            "cin" => "JA12345",
+        ]);
 
-        $stagiaires = User::where('role', 'stagiaire')->get();
-        foreach ($stagiaires as $stagiaire) {
-            \App\Models\Stagiaire::factory()->create();
-        }
+        User::factory()->create([
+            "prenom" => "jane",
+            "nom" => "doe",
+            "email" => "janedoe@gmail.com",
+            "password" => bcrypt("123456"),  // Use bcrypt for password
+            "role" => "administrateur",
+            "cin" => "JA67890",
+        ]);
 
-        $formateurs = User::where('role', 'formateur')->get();
-        foreach ($formateurs as $formateur) {
-            \App\Models\Formateur::factory()->create();
-        }
+        // Create Stagiaire records
+        Stagiaire::factory(10)->create();
 
-        $administrateurs = User::where('role', 'administrateur')->get();
-        foreach ($administrateurs as $administrateur) {
-            \App\Models\Administrateur::factory()->create();
-        }
+        // Seed demandes
+        Demande::factory(20)->create();
 
-        $groupes = Groupe::all();
-        $formateurs = \App\Models\Formateur::all();
-        foreach($groupes as $index => $groupe){
-            \App\Models\Module::factory()->create();
-        }
+        // $formateurs = User::where('role', 'formateur')->get();
+        // foreach ($formateurs as $formateur) {
+        //     \App\Models\Formateur::factory()->create();
+        // }
+
+        // $administrateurs = User::where('role', 'administrateur')->get();
+        // foreach ($administrateurs as $administrateur) {
+        //     \App\Models\Administrateur::factory()->create();
+        // }
+
+        // $groupes = Groupe::all();
+        // $formateurs = \App\Models\Formateur::all();
+        // foreach ($groupes as $index => $groupe) {
+        //     \App\Models\Module::factory()->create();
+        // }
 
         // Seed quizzes
-        \App\Models\Quiz::factory(10)->create();
+        //\App\Models\Quiz::factory(10)->create();
 
         // Seed questions
-        \App\Models\Question::factory(30)->create();
+        //\App\Models\Question::factory(30)->create();
 
         // Seed options
-        \App\Models\Option::factory(50)->create();
+        // \App\Models\Option::factory(50)->create();
 
 
         // Seed quiz_question pivot table
-        $quizzes = Quiz::all();
-        $questions = Question::all();
-        foreach ($quizzes as $quiz) {
-            $quiz->questions()->attach($questions->random(rand(5, 10))->pluck('id'));
-        }
+        // $quizzes = Quiz::all();
+        // $questions = Question::all();
+        // foreach ($quizzes as $quiz) {
+        //     $quiz->questions()->attach($questions->random(rand(5, 10))->pluck('id'));
+        // }
 
         // Seed demandes
-        Demande::factory()->create(20);
+        // Demande::factory()->create(20);
     }
 }
