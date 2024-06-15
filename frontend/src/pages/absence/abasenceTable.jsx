4fcf364fd33e80ddabcd91adb356cbc4e6689c9e
ila36IX <jbariali002@gmail.com>
2024-06-15 07:29:50 +0100

    Adding form to select which group to mark its absence

--- frontend/src/pages/absence/abasenceTable.jsx
+++ frontend/src/pages/absence/abasenceTable.jsx
@@ -1,6 +1,6 @@
 import {
   Button,
-  Typography,
+  Typography
 } from "@material-tailwind/react";
 import { Switch } from '@headlessui/react'
 import { useState } from "react";
@@ -16,6 +16,15 @@ const student = {
   isPrevAbsente: true,
 }
 
+/**
+ * Row: is a component that represent a student row in that table of absence
+ * @sudent: is an object of student, it must have those keys
+ *  id, name, isPrevAbsente
+ * @rowNum: It should give the index of that student in the array
+ *  this param will help in styling the table
+ * @marked: Optional parameter if it set to true this student absente state
+ *  of that day will not allowed to set (the switch will be disabled).
+ */
 function Row({ student, rowNum, marked = false }) {
   return <tr key={student.id}>
     <td
@@ -61,6 +70,10 @@ function Row({ student, rowNum, marked = false }) {
   </tr>
 }
 
+/**
+ * AbsenceTable: Componente represening the table itself, will have
+ * as a children Rows components
+ */
 function AbsenceTable({ children }) {
   return (
     <div className="-mx-4 mt-10 ring-1 ring-gray-300 sm:mx-6 md:mx-0 md:rounded-lg">
@@ -92,29 +105,13 @@ function AbsenceTable({ children }) {
   )
 }
 
-export default function Absence() {
-  return <div className="px-4  py-10 sm:px-6 lg:px-8">
-    <div className="sm:flex sm:text-center lg:text-start sm:items-center">
-      <div className="sm:flex-auto ">
-        <Typography variant="h4" color="blue-gray" className="font-semibold mb-4">
-          Saisie des absences
-        </Typography>
-      </div>
-    </div>
-    <AbsenceTable>
-      <Row student={student} marked={true} />
-      <Row student={student} marked={true} />
-      <Row student={student} />
-      <Row student={student} />
-    </AbsenceTable>
-    <div className="mt-4 flex sm:flex-none sm:justify-center lg:justify-end">
-      <Button color="blue-gray">
-        Ajouter l'événement
-      </Button>
-    </div>
-  </div>
-}
-
+/**
+ * Toggle: is the switch that will be used to set a
+ * student absente state
+ * @marked: if set to false the switch will work
+ * and if set to true the state of that switch will be
+ * read only
+ */
 function Toggle({ marked }) {
   const [enabled, setEnabled] = useState(true)
   return (
@@ -129,7 +126,6 @@ function Toggle({ marked }) {
         cursor: marked ? "not-allowed" : "pointer"
       }}
     >
-      <span className="sr-only">Use setting</span>
       <span
         className={classNames(
           enabled ? 'translate-x-5' : 'translate-x-0',
@@ -168,3 +164,185 @@ function Toggle({ marked }) {
     </Switch>
   )
 }
+
+const GroupsList = {
+  Filieres: [
+    {
+      "id": 1,
+      "name": "filier1",
+      "specialite": "Technicien Spécialisé",
+      "created_at": "2024-06-11T01:23:34.000000Z",
+      "updated_at": "2024-06-11T01:23:34.000000Z"
+    },
+    {
+      "id": 2,
+      "name": "filier2",
+      "specialite": "Technicien Spécialisé",
+      "created_at": "2024-06-11T01:23:34.000000Z",
+      "updated_at": "2024-06-11T01:23:34.000000Z"
+    }],
+  Groupes: [
+    {
+      "id": 1,
+      "name": "groupe1-1",
+      "code": "M714",
+      "annee": "2A",
+      "filiere_id": 1,
+      "created_at": "2024-06-11T01:23:34.000000Z",
+      "updated_at": "2024-06-11T01:23:34.000000Z"
+    },
+    {
+      "id": 2,
+      "name": "groupe1-2",
+      "code": "M714",
+      "annee": "2A",
+      "filiere_id": 1,
+      "created_at": "2024-06-11T01:23:34.000000Z",
+      "updated_at": "2024-06-11T01:23:34.000000Z"
+    },
+    {
+      "id": 3,
+      "name": "groupe2-1",
+      "code": "M264",
+      "annee": "1A",
+      "filiere_id": 2,
+      "created_at": "2024-06-11T01:23:34.000000Z",
+      "updated_at": "2024-06-11T01:23:34.000000Z"
+    },
+    {
+      "id": 4,
+      "name": "groupe2-2",
+      "code": "M264",
+      "annee": "1A",
+      "filiere_id": 2,
+      "created_at": "2024-06-11T01:23:34.000000Z",
+      "updated_at": "2024-06-11T01:23:34.000000Z"
+    },
+    {
+      "id": 5,
+      "name": "groupe2-3",
+      "code": "M264",
+      "annee": "1A",
+      "filiere_id": 2,
+      "created_at": "2024-06-11T01:23:34.000000Z",
+      "updated_at": "2024-06-11T01:23:34.000000Z"
+    }
+  ]
+}
+
+/**
+ * SelectGroup - this componenet will be used to set the group mark 
+ *  it absente state or previewe its previes absente states if it was
+ *  already marked.
+ */
+const SelectGroup = ({ setGroupeName }) => {
+  const [filiere, setFilier] = useState(GroupsList.Filieres[0]?.id);
+  const [groupe, setGroupe] = useState(groupsByFiliere(filiere)[0].id);
+
+  function groupsByFiliere(filier_id) {
+    return GroupsList.Groupes.filter((g) => g.filiere_id === Number(filier_id));
+  }
+
+  function hondleFilierChange(e) {
+    setFilier(e.target.value);
+    if (groupsByFiliere(e.target.value).length > 0) {
+      setGroupe(groupsByFiliere(e.target.value)[0]?.id);
+    }
+  }
+
+  function setSelectedGroupName() {
+    const ChosenGroupe = GroupsList.Groupes.find((g) => g.id === Number(groupe));
+    setGroupeName(ChosenGroupe.name);
+  }
+  return (
+    <div>
+      <Typography variant="h2" color="blue-gray" className="font-semibold mb-4">
+        Choiser le groupe!
+      </Typography>
+      <div className="mt-10 w-96 items-end justify-center" >
+        <div className="sm:col-span-2">
+          <Typography variant="h4" color="blue-gray" className="font-semibold mb-4">
+            Choiser le filiere
+          </Typography>
+          <div className="mt-4 mb-5">
+            <select
+              name="country"
+              autoComplete="country-name"
+              className="block w-full rounded-md border-0 px-5 py-3 bg-white text-gray-900 shadow-sm ring-1 ring-inset  focus:ring-inset focus:ring-[#607D8B]"
+              value={filiere}
+              onChange={(e) => hondleFilierChange(e)}
+            >
+              {GroupsList.Filieres.map((f) => {
+                return <option key={f.id} value={f.id}>{f.name}</option>
+              })}
+            </select>
+          </div>
+        </div>
+        <div>
+          <Typography variant="h4" color="blue-gray" className="font-semibold mb-4">
+            Choiser le Group
+          </Typography>
+          <div className="mt-4">
+            <select
+              name="country"
+              autoComplete="country-name"
+              className="block w-full rounded-md border-0 px-5 py-3 bg-white text-gray-900 shadow-sm ring-1 ring-inset  focus:ring-inset focus:ring-[#607D8B]"
+              value={groupe}
+              onChange={(e) => setGroupe(e.target.value)}
+            >
+              {groupsByFiliere(filiere).map((g) => {
+                return <option key={g.id} value={g.id}>{g.name}</option>
+              })}
+            </select>
+          </div>
+
+          <Button color="blue-gray" className="mt-10 w-full" onClick={setSelectedGroupName}>
+            Go to table
+          </Button>
+        </div>
+      </div >
+    </div>
+  );
+};
+
+/**
+ * Absence: this is the actual page that will be renderd in the absence section
+ */
+function Absence() {
+  const [GroupeName, setGroupeName] = useState(null);
+
+  return <div className="px-4  py-10 sm:px-6 lg:px-8">
+    {GroupeName ?
+      <>
+        <div className="sm:flex sm:text-center lg:text-start sm:items-center">
+          <div className="sm:flex-auto ">
+            <Typography variant="h2" color="blue-gray" className="font-semibold mb-4">
+              Saisie la absence
+            </Typography>
+            <Typography variant="h6" color="blue-gray" className="font-semibold mb-4">
+              Vous marquez désormais les absences du groupe <span className="underline">{GroupeName}</span>.
+            </Typography>
+
+          </div>
+        </div>
+        <AbsenceTable>
+          <Row student={student} marked={true} />
+          <Row student={student} marked={true} />
+          <Row student={student} />
+          <Row student={student} />
+        </AbsenceTable>
+        <div className="mt-4 flex sm:flex-none gap-2 sm:justify-center lg:justify-end">
+          <a href="#buttons-with-link">
+            <Button variant="text" onClick={() => setGroupeName(null)}> Changer le groupe?</Button>
+          </a>
+          <Button color="blue-gray">
+            Mark absence
+          </Button>
+        </div>
+      </>
+      : <SelectGroup setGroupeName={setGroupeName} />
+    }
+  </div>
+}
+
+export default Absence;
